2d16e96a9950e3d4edf36ec587c9b104c59d7f55
prans7590 <prans7590@gmail.com>
2022-12-16 01:58:49 +0530

    all commit

---
+++ Demovar.java
@@ -0,0 +1,16 @@
+public class Demovar {
+
+    // it has
+
+    int aWheel;
+    String brandName;
+
+    // it does something
+    public void breakCar() {
+        System.out.println("break");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

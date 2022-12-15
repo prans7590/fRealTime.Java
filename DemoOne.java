a861144a07b4b964ae8a0bf5acd45a0307642923
prans7590 <prans7590@gmail.com>
2022-12-15 06:14:35 +0530

    DemoOne.java added

---
+++ DemoOne.java
@@ -0,0 +1,10 @@
+package Test1;
+
+public class DemoOne {
+
+	public static void main(String[] args) {
+
+      System.out.println("this is test1.............");
+	}
+
+}
--- one.java
+++
@@ -1,5 +0,0 @@
-public class one {
-    public static void main(String[] args) {
-        System.err.println("demo to git push cmd ");
-    }
-}

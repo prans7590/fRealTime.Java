141a120c9eff5cdf4503e20f6c4b69f32b14ec9b
prans7590 <prans7590@gmail.com>
2022-12-16 03:39:47 +0530

    ablout variable updated one

--- Demovar.java
+++ Demovar.java
@@ -1,15 +1,36 @@
-public class Demovar {
+// variable in java example1
+//identifier it is anamein java program
+//Tt can be a class name, mthord name and variable name , label name
 
-    // it has
+//Rules for writing an identifier 
+//...............Rules.....................
+//Rule1 : the only allowed characters in java identifiers are
+// a to z , A to Z , 0 to 9  , _(underscore),$
+//Rule2 if we use any other character it would result 
 
+//int *8e8 = 10 ; (invalid)
+//int total = 10; (valid)
+//int Total # = 4; (invalid
+//int asmap1 = 100; (valid)
+//Rule 3  int 1amap = 33(intvalid) it will not allowed to start with number
+//Rule 5  int number = 8; (valid) , int Number = 3 ; it is treated as case sensitive
+//Rule6 int oneNumber= 3 ; (valid) it follow camel case if you attach two differt word
+
+class Student {
+    // has a part
+    int rollNo;
+    String studentName;
+
+}
+
+class Car {
+    // has a part
     int aWheel;
-    String brandName;
+    String bransName;
 
-    // it does something
-    public void breakCar() {
-        System.out.println("break");
-    }
+}
 
+public class Demovar {
     public static void main(String[] args) {
 
     }

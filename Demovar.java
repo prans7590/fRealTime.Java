1c6a70c81d0ff1920d2070ed07452459e96a1ccf
prans7590 <prans7590@gmail.com>
2022-12-16 04:01:40 +0530

    updated one

--- Demovar.java
+++ Demovar.java
@@ -4,18 +4,18 @@
 
 //Rules for writing an identifier 
 //...............Rules.....................
-//Rule1 : the only allowed characters in java identifiers are
+//Rule1 :The only allowed characters in java identifiers are
 // a to z , A to Z , 0 to 9  , _(underscore),$
-//Rule2 if we use any other character it would result 
-
-//int *8e8 = 10 ; (invalid)
+//Rule2 :  if we use any other character we will get error
+//int *8e8 = 10 ; (invalid) we use ( $ , _ )only
 //int total = 10; (valid)
-//int Total # = 4; (invalid
+//int Total# = 4; (invalidwe use ( $ , _ )only
 //int asmap1 = 100; (valid)
 //Rule 3  int 1amap = 33(intvalid) it will not allowed to start with number
 //Rule 5  int number = 8; (valid) , int Number = 3 ; it is treated as case sensitive
 //Rule6 int oneNumber= 3 ; (valid) it follow camel case if you attach two differt word
-
+//Rule7 we cant use reserve word for identifier
+// int if = 3939; (invalid) it foll unser rule7
 class Student {
     // has a part
     int rollNo;

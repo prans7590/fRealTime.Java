0ee9be67ed50d2fd93359a15ce9aef8b66b184cf
prans7590 <prans7590@gmail.com>
2022-12-15 19:08:49 +0530

    added1

--- Core.java
+++ Core.java
@@ -1,5 +1,17 @@
-public class Core {
+class example1 {
     public static void main(String[] args) {
         System.out.println("way to core java");
     }
 }
+
+// command to execute in cmd
+
+// javac filename.java
+// after this command once class file generate
+// java classfilename
+
+// without generating class file your can execute using (java filename)
+
+// Note there is not compulsory to match the java file name and class name
+// if you are putting public before class name you have to
+// put the class name and file name same

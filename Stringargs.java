424407dfdef3acb28fcd91a15c228fa9e95c2d26
prans7590 <prans7590@gmail.com>
2022-12-15 21:43:49 +0530

    new file i added

---
+++ Stringargs.java
@@ -0,0 +1,20 @@
+public class Stringargs {
+
+    public static void main(String[] args) {
+
+        System.out.println("hi pranav kumar");
+        System.out.println(args[0]);
+        System.out.println(args[3]);
+    }
+}
+
+// why to write String ars[];
+// At the time execution we can give perementer to String args[]
+// as we know that to runs code we are putting cmd
+// javac Stringargs.java
+// at the time of execution we can give parameter to String agrs[]
+// like java Stringargs "pranav", "kumar" after putting this we will output liek
+
+// hi pranav kuamr
+// pranav
+// kumar

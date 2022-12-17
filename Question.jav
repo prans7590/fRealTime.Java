21e5e348172e483f3fefb6591cbdec41b2015310
prans7590 <prans7590@gmail.com>
2022-12-18 05:06:46 +0530

    updated

--- Question.jav
+++
@@ -1,7 +0,0 @@
-public Class Question{
-    public static void main(String[] args) {
-        
-     //Arithmetic operators 
-    
-    }
-}
---
+++ Question.java
@@ -0,0 +1,34 @@
+public class Question {
+    public static void main(String[] args) {
+
+        // Arithmetic operators
+
+        int a = 39;
+        int b = 37;
+
+        System.out.println("the value of a and b is " + (a + b));
+        System.out.println("the value of a and b is " + (a - b));
+        System.out.println("the value of a and b is " + (a * b));
+        System.out.println("the value of a and b is " + (a / b));
+        System.out.println("the value of a and b is " + (a % b));
+
+        // Logical operators
+
+        // for and operators it will rep as &&
+        System.out.println("the x and x combination :" + (true && true));
+        System.out.println("the x and y combination :" + (true && false));
+        System.out.println("the y and x combination :" + (false && false));
+        System.out.println("the y and y combination :" + (false && true));
+
+        // for or operators ||
+        System.out.println("the x and x combination :" + (true || true));
+        System.out.println("the x and y combination :" + (true || false));
+        System.out.println("the y and x combination :" + (false || false));
+        System.out.println("the y and y combination :" + (false || true));
+
+        // for not operators sym is !
+        System.out.println("the not of true is : " + (!true));
+        System.out.println("the not of true is : " + (!false));
+
+    }
+}

8dfbcc24528b92df4f9b7ffaafa928982f4a6d5f
prans7590 <prans7590@gmail.com>
2022-12-24 01:49:49 +0530

    updated

---
+++ loopcs.java
@@ -0,0 +1,23 @@
+import javax.swing.SpinnerDateModel;
+
+public class loopcs {
+    public static void main(String[] args) {
+        int i = 1_2;
+        System.out.println(i);
+        int a, b, c;
+        int d = a = b = c = 22;// compound statement chain assignment or Shorthand Operators
+        System.out.println(d);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        int s = (20 > 4) ? 2 : 39;
+        System.out.println(s);
+
+        int jj = 29;
+        int kk = 3;
+        float m = ((float) jj > (float) kk) ? 2 : 4;
+        System.out.println(m);
+    }
+
+}

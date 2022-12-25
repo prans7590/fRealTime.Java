5f37199456ed5a2bc3708b843efe1005c52b4c93
prans7590 <prans7590@gmail.com>
2022-12-26 01:00:28 +0530

    updated

---
+++ Oops.java
@@ -0,0 +1,23 @@
+//class is blueprint of an object
+class StudentA {
+
+    // Has or attribute
+    int stdId;
+    String name;
+    int age;
+
+    void playingCricket() {
+        System.out.print("Student is playing game");
+    }
+
+    void sleeping() {
+        System.out.println("Student is p");
+    }
+}
+
+public class Oops {
+    static public void main(String[] args) {
+        StudentA studentDetail = new StudentA();
+    }
+
+}
---
+++ pp.java
@@ -0,0 +1,19 @@
+class a {
+    int a;
+    int b;
+
+}
+
+public class pp {
+    public static void main(String[] args) {
+        int x;
+
+        int i = 0;
+        for (int j = 0; j < 3; j++) {
+            i = i + j;
+            System.out.println(i);
+        }
+        System.out.println(args.length);
+    }
+
+}

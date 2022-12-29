721c3033f3592eb0812269e3f3214e71b452ffe9
prans7590 <prans7590@gmail.com>
2022-12-30 04:12:12 +0530

    updated

---
+++ Scanner2darr.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Scanner2darr {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a[][] = new int[2][4];
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("enter you class Sec number in digit " + i);
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.println("enter you student age in digit" + j);
+                a[i][j] = sc.nextInt();
+
+            }
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.println(a[i][j]);
+
+            }
+        }
+
+    }
+
+}
--- ScannerArrr.java
+++ ScannerArrr.java
@@ -5,8 +5,8 @@ public class ScannerArrr {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String[] a = new String[5];
-        for (int i = 0; i < 5; i++) {
+        String[] a = new String[10];
+        for (int i = 0; i < a.length; i++) {
             System.out.println("plz enter your name");
             a[i] = sc.nextLine();
 

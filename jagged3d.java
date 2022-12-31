e7aa68ff4f49bbcddd01ed21b0972a274e2f250d
prans7590 <prans7590@gmail.com>
2022-12-31 05:56:53 +0530

    updated

--- jagged3d.java
+++ jagged3d.java
@@ -5,20 +5,45 @@ public class jagged3d {
         int $3[][][] = new int[1][2][4];
         Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < $3.length; i++) {
-            System.out.println("plz enter class number");
-            for (int j = 0; j < $3[i].length; j++) {
-                System.out.println("plz enter class number in digital number");
-                for (int k = 0; k < $3[i][j].length; k++) {
+        // for (int i = 0; i < $3.length; i++) {
+        // System.out.println("plz enter class number");
+        // for (int j = 0; j < $3[i].length; j++) {
+        // System.out.println("plz enter class number in digital number");
+        // for (int k = 0; k < $3[i][j].length; k++) {
+        // System.out.println("plz enter your Student Roll number");
+        // $3[i][j][k] = sc.nextInt();
+        // }
+        // }
+        // }
+        // for (int i = 0; i < $3.length; i++) {
+        // for (int j = 0; j < $3[i].length; j++) {
+        // for (int k = 0; k < $3[i][j].length; k++) {
+        // System.out.println("the clg +" + i + "the class" + j + " student rno " + k +
+        // ":" + $3[i][j][k]);
+        // }
+        // }
+        // }
+
+        System.out.println("..............jagged ...............array.........");
+        int $2[][][] = new int[1][2][];
+        $2[0][0] = new int[2];
+        $2[0][1] = new int[5];
+
+        for (int i = 0; i < $2.length; i++) {
+            System.out.println("clg detail " + i);
+            for (int j = 0; j < $2[i].length; j++) {
+                System.out.println("cls detail " + j);
+                for (int k = 0; k < $2[i][j].length; k++) {
                     System.out.println("plz enter your Student Roll number");
-                    $3[i][j][k] = sc.nextInt();
+                    $2[i][j][k] = sc.nextInt();
                 }
             }
         }
-        for (int i = 0; i < $3.length; i++) {
-            for (int j = 0; j < $3[i].length; j++) {
-                for (int k = 0; k < $3[i][j].length; k++) {
-                    System.out.println("the clg +" + i + "the class" + j + " student rno " + k + ":" + $3[i][j][k]);
+
+        for (int i = 0; i < $2.length; i++) {
+            for (int j = 0; j < $2[i].length; j++) {
+                for (int k = 0; k < $2[i][j].length; k++) {
+                    System.out.println("the clg +" + i + "the class" + j + " student rno " + k + ":" + $2[i][j][k]);
                 }
             }
         }

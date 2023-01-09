4bf5dfcce8eab652318b321d006fc17acfde0d6a
prans7590 <prans7590@gmail.com>
2023-01-09 13:44:36 +0530

    updated

---
+++ rev_trv_arr.java
@@ -0,0 +1,9 @@
+public class rev_trv_arr {
+    public static void main(String[] args) {
+
+        int arr[] = { 44, 4, 4, 4, 4, 4, 4, 4, 4, };
+        for (int i = arr.length - 1; i >= 0; i--)
+            System.out.println(arr[i]);
+    }
+
+}
